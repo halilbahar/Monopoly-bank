ab2bef0f046fe81207535a2340527812383c422d
halilbahar <halil.s.bahar@gmail.com>
2020-12-17 00:30:36 +0100

    Setup routing

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,22 @@
 import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
+import { HomeComponent } from './pages/home/home.component';
+import { LeaderboardComponent } from './pages/leaderboard/leaderboard.component';
+import { MultiTransactionComponent } from './pages/multi-transaction/multi-transaction.component';
+import { OverviewComponent } from './pages/overview/overview.component';
+import { SingleTransactionComponent } from './pages/single-transaction/single-transaction.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', component: HomeComponent },
+  {
+    path: '', children: [
+      { path: 'overview', component: OverviewComponent },
+      { path: 'single-transaction', component: SingleTransactionComponent },
+      { path: 'multi-transaction', component: MultiTransactionComponent },
+      { path: 'leaderboard', component: LeaderboardComponent }
+    ]
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
