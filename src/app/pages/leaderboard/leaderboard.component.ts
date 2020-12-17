0d450f441cd58282747348e79048ad561300c212
halilbahar <halil.s.bahar@gmail.com>
2020-12-17 17:04:38 +0100

    List all player in a leaderboard

--- src/app/pages/leaderboard/leaderboard.component.ts
+++ src/app/pages/leaderboard/leaderboard.component.ts
@@ -1,15 +1,36 @@
 import { Component, OnInit } from '@angular/core';
+import { Observable } from 'rxjs';
+import { map } from 'rxjs/operators';
+import { PlayerService } from 'src/app/core/services/player.service';
+import { Player } from 'src/app/shared/models/player.module';
 
 @Component({
   selector: 'app-leaderboard',
   templateUrl: './leaderboard.component.html',
   styleUrls: ['./leaderboard.component.scss']
 })
-export class LeaderboardComponent implements OnInit {
+export class LeaderboardComponent {
 
-  constructor() { }
+  players: Observable<(Player & { rank: number })[]>;
 
-  ngOnInit(): void {
+  constructor(
+    public playerService: PlayerService
+  ) {
+    this.players = this.playerService.players.pipe(
+      map(players => {
+        let newPlayers = [...players] as (Player & { rank: number })[];
+        newPlayers.sort((a, b) => b.balance - a.balance);
+        newPlayers.forEach((player, index) => {
+          const previousPlayer = newPlayers[index - 1];
+          if (previousPlayer && previousPlayer.balance === player.balance) {
+            player.rank = previousPlayer.rank;
+          } else {
+            player.rank = index + 1;
+          }
+        });
+
+        return newPlayers;
+      })
+    );
   }
-
 }
