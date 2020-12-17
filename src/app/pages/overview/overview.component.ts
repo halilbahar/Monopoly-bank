ab1a90b81afefbb28252a2f17fe122aac2e0e982
halilbahar <halil.s.bahar@gmail.com>
2020-12-17 14:07:38 +0100

    List all players in overview

--- src/app/pages/overview/overview.component.ts
+++ src/app/pages/overview/overview.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { PlayerService } from 'src/app/core/services/player.service';
 
 @Component({
   selector: 'app-overview',
@@ -7,7 +8,9 @@ import { Component, OnInit } from '@angular/core';
 })
 export class OverviewComponent implements OnInit {
 
-  constructor() { }
+  constructor(
+    public playerService: PlayerService
+  ) { }
 
   ngOnInit(): void {
   }
