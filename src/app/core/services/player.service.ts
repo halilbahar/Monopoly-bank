b7a4ace35a73e24c8272f41ba15c59432a5578bf
halilbahar <halil.s.bahar@gmail.com>
2021-05-31 01:48:22 +0200

    prefix localstorage

--- src/app/core/services/player.service.ts
+++ src/app/core/services/player.service.ts
@@ -9,24 +9,24 @@ export class PlayerService {
   players: BehaviorSubject<Player[]>;
 
   constructor() {
-    const playersStorage = localStorage.getItem('players');
+    const playersStorage = localStorage.getItem('monopoly-bank.players');
     const initialValue = playersStorage ? JSON.parse(playersStorage) : [];
     this.players = new BehaviorSubject(initialValue);
   }
 
   setPlayers(players: Player[]): void {
     this.players.next(players);
-    localStorage.setItem('players', JSON.stringify(this.players.value));
+    localStorage.setItem('monopoly-bank.players', JSON.stringify(this.players.value));
   }
 
   changePlayer(changedPlayer: Player): void {
     const player = this.players.value.find(player => player.name === changedPlayer.name);
     player.balance = changedPlayer.balance;
-    localStorage.setItem('players', JSON.stringify(this.players.value));
+    localStorage.setItem('monopoly-bank.players', JSON.stringify(this.players.value));
   }
 
   clearGame(): void {
     this.players.next([]);
-    localStorage.removeItem('players');
+    localStorage.removeItem('monopoly-bank.players');
   }
 }
